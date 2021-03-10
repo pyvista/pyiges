327bf2cb84ee95a6266e2c24f0ebf4f05ee815a0
Alex Kaszynski <akascap@gmail.com>
2021-03-10 11:12:46 -0700

    bump version to 0.1.5

--- pyiges/_version.py
+++ pyiges/_version.py
@@ -1,6 +1,6 @@
 """ pyiges version """
 # major, minor, patch, -extra
-version_info = 0, 1, 4
+version_info = 0, 1, 5
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
