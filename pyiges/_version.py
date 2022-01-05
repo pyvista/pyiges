a4480f5888d6b751c1d22e6aac0587498e6eab85
Alex Kaszynski <akascap@gmail.com>
2022-01-05 08:38:13 -0700

    use dev on main branch

--- pyiges/_version.py
+++ pyiges/_version.py
@@ -1,6 +1,21 @@
-""" pyiges version """
+"""Version info for pyiges.
+
+On the ``main`` branch, use 'dev0' to denote a development version.
+For example:
+
+version_info = 0, 27, 'dev0'
+
+---
+
+When generating pre-release wheels, use '0rcN', for example:
+
+version_info = 0, 28, '0rc1'
+
+Denotes the first release candidate.
+
+"""
 # major, minor, patch, -extra
-version_info = 0, 2, 0
+version_info = 0, 3, 'dev0'
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
