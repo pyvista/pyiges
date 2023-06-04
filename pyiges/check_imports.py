5a143ce91a53865545114b26dfb6351b7581765e
Clemens Löbner <17496175+mikamove@users.noreply.github.com>
2023-06-04 13:36:12 +0200

    improved import error detection

--- pyiges/check_imports.py
+++ pyiges/check_imports.py
@@ -4,12 +4,12 @@ try:
     import geomdl
     from pyvista._vtk import vtkAppendPolyData
     _IS_FULL_MODULE = True
-except ModuleNotFoundError as exc:
+except (ModuleNotFoundError, ImportError) as exc:
     pyvista = None
     geomdl = None
     vtkAppendPolyData = None
     _IS_FULL_MODULE = False
-    _PROBLEM_MSG = 'Module \'{}\' not found, to support this feature '\
+    _PROBLEM_MSG = 'Import from \'{}\' failed, to support this feature '\
         'please install pyiges[full]'.format(exc.name)
 
 def assert_full_module_variant(inner_func):
