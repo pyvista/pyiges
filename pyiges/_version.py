ec33a9941cc1cdbb6c081bae4ae614283b046cb8
Bane Sullivan <bane.sullivan@kitware.com>
2023-07-05 08:40:41 -0600

    Bump version

--- pyiges/_version.py
+++ pyiges/_version.py
@@ -15,7 +15,7 @@ Denotes the first release candidate.
 
 """
 # major, minor, patch, -extra
-version_info = 0, 3, 0
+version_info = 0, 3, 1
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
